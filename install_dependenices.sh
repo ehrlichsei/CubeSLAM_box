eacacab0efe20aa25a1da3c7b9ae3946166709c8
ehrlichsei <hohofyl@163.com>
2021-03-31 19:13:46 +0200

    add vocabulary.tar and integrate in install_dependencies.sh

--- install_dependenices.sh
+++ install_dependenices.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Configuring and building object_slam/Thirdparty/g2o ..."
 cd object_slam/Thirdparty/g2o
 mkdir build
 cd build
@@ -7,6 +8,7 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j2
 cd ../../../..
 
+echo "Configuring and building orb_object_slam/Thirdparty/g2o ..."
 cd orb_object_slam/Thirdparty/g2o
 mkdir build
 cd build
@@ -14,9 +16,16 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j2
 cd ../../../..
 
+echo "Configuring and building orb_object_slam/Thirdparty/DBoW2 ..."
 cd orb_object_slam/Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j2
 cd ../../../..
+
+echo "Uncompress vocabulary ..."
+
+cd orb_object_slam/Vocabulary
+tar -xf ORBvoc.txt.tar.gz
+cd ..
